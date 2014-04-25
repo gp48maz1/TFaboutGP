1611b1fd0c77693729537a4bea6116e0f15bfef1
Gary Mazzone Peters <gary.m.peters@vanderbilt.edu>
2014-04-25 15:37:13 -0500

    Further updates, not sure what to say

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,14 @@ app = Flask(__name__)
 def home():
 	return render_template('home.html')
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/other')
+def other():
+	return render_template('other.html')
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
-	
